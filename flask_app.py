b514411ec61418bfc548d28705d9ac77d7147995
Rai <rai@ingmechs.com>
2019-09-23 22:45:50 +0200

    Fix Timezones

--- flask_app.py
+++ flask_app.py
@@ -53,6 +53,7 @@ def sensor(sensor_id):
     return render_template('chart2.html',
                            values=values[::-1],
                            labels=labels[::-1],
+                           updated=labels[::-1][-1],
                            max=4096,
                            sensors=sensor_list,
                            title=' AIO' +
