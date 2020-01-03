75c3dfb66ce797fc2c20ebfb78634964ea0df5b1
Rai <rai@ingmechs.com>
2020-01-03 21:12:12 +0100

    protect

--- app/users/routes.py
+++ app/users/routes.py
@@ -9,6 +9,7 @@ import logging
 users = Blueprint('users', __name__)
 
 @users.route("/register", methods=['GET','POST'])
+@login_required
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('main.home'))
