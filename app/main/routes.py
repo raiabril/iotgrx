ff80d4b374bd2e126bcb2b99c7584f9e65fbd96c
Rai <rai@ingmechs.com>
2020-03-25 00:46:56 +0100

    last update format

--- app/main/routes.py
+++ app/main/routes.py
@@ -67,7 +67,7 @@ def sensor(sensor_id):
             colorFill=redFill
             colorLine=redLine
 
-        updated_value = "{:10.3f} ({})".format(updated_value*sensor.a1 + sensor.a0, updated_value)
+        updated_value = "{:10.2f}".format(updated_value*sensor.a1 + sensor.a0)
         values = [x*sensor.a1 + sensor.a0 for x in values]
         values = ["{:10.3f}".format(x) for x in values]
 
