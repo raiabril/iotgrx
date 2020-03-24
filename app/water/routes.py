29b818c2ea4bdfd51c467ca637db3b5e7bb5a9ed
Rai <rai@ingmechs.com>
2020-03-24 22:26:14 +0100

    added last update

--- app/water/routes.py
+++ app/water/routes.py
@@ -12,9 +12,10 @@ water = Blueprint('water', __name__)
 def check(device_code):
     device = Device.query.filter_by(code=device_code).first()
     devices = Device.query.filter_by(active=1).all()
-    requests = WaterRequest.query.filter_by(pending=True).order_by(WaterRequest.date_created.desc()).all()
+    requests = WaterRequest.query.filter_by(pending=True).order_by(WaterRequest.date_created.asc()).all()
     logs = WaterLog.query.filter_by(device_code=device.code).order_by(WaterLog.date_created.desc()).limit(10).all()
     form = WaterForm()
+    event = Event.query.filter_by(device_code=device.code).order_by(Event.date_created.desc()).first()
 
     if form.validate_on_submit():
         duration = form.duration.data
@@ -30,4 +31,5 @@ def check(device_code):
                             logs=logs,
                             form=form,
                             device=device,
-                            devices=devices)
+                            devices=devices,
+                            event=event)
