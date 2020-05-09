170b5a284923c19983ccd78d4929d8871f1a0f9b
Rai <rai@ingmechs.com>
2020-05-09 21:27:58 +0200

    track modifications

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ class Config:
     SECRET_KEY='dadc98d2ce2925035ffc2bdc6ce3c190'
     SQLALCHEMY_POOL_RECYCLE = 3600
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db/iotHome.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
