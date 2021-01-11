ea65b7b984dbff26ed493359642ed0da7d6495ba
Rai <rai@ingmechs.com>
2021-01-11 10:28:09 +0100

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -180,7 +180,7 @@ def sensor(sensor_id):
                 df_mean = df.groupby(pd.Grouper(freq=freq)).mean()
                 df_max = df.groupby(pd.Grouper(freq=freq)).max()
                 df_min = df.groupby(pd.Grouper(freq=freq)).min()
-                df_rolling = df.rolling(24*2*4, center=True).mean()
+                df_rolling = df.rolling(24, center=True).mean()
                 df_min.columns = ['value_min']
                 df_max.columns = ['value_max']
                 df_mean.columns = ['avg_mean']
