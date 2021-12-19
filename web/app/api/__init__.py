ca2403e2db68de5ea3ad2a761d6fa52019ed896a
raiabril <rai@ingmechs.com>
2021-12-19 18:55:32 +0100

    Removed Flask project

--- web/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-
-bp = Blueprint('api', __name__)
-bp_api = Api(bp)
-
-from app.api import events, users, errors, tokens, water, sensors, devices, sleep
